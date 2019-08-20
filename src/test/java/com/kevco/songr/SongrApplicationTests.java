ff9a242831a8bb77142095f74700ea1892d2a385
Kevin Couture <kcouture939@gmail.com>
2019-08-20 10:39:43 -0700

    Lab Tests passing, routes functioning.

---
+++ src/test/java/com/kevco/songr/SongrApplicationTests.java
@@ -0,0 +1,27 @@
+package com.kevco.songr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SongrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test public void testReverse() {
+		assertEquals("test1 test2 > ","test2 test1", HomeController.reverseString("test1 test2"));
+		assertEquals("hello goodbye > ","goodbye hello", HomeController.reverseString("hello goodbye"));
+	}
+
+	@Test public void testCapitalize() {
+		assertEquals("test > TEST", "TEST", HomeController.capitalizeString("test"));
+		assertEquals("tEsE > TEST", "TEST", HomeController.capitalizeString("tEsT"));
+	}
+
+}
